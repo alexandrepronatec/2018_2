c9659bd13bdf32ad8a23429ed47ab85fc8960ef6
AlexandreCajamos <alexandre.cajamos@gmail.com>
2018-09-20 12:15:39 -0300

    Aula 02 - Calculadora

---
+++ Aulas/Aula_02/operacoes.js
@@ -0,0 +1,31 @@
+
+
+//Aqui podemos adicionar códigos escritos em Javascript. Nesta aula, faremos as funções Som(), Sub(), Div(), Mlt().
+
+function Som(){
+	var a = parseInt(document.getElementById('A').value);
+    var b = parseInt(document.getElementById('B').value);
+    var resultado = a+b;
+    alert("("+a+"+"+b+") = " + resultado);
+}
+
+function Sub(){
+	var a = parseInt(document.getElementById('A').value);
+    var b = parseInt(document.getElementById('B').value);
+    var resultado = a-b;
+    alert("("+a+"-"+b+") = " + resultado);
+}
+
+function Div(){
+	var a = parseInt(document.getElementById('A').value);
+    var b = parseInt(document.getElementById('B').value);
+    var resultado = a/b;
+    alert("("+a+"/"+b+") = " + resultado);
+}
+
+function Mlt(){
+	var a = parseInt(document.getElementById('A').value);
+    var b = parseInt(document.getElementById('B').value);
+    var resultado = a*b;
+    alert("("+a+"*"+b+") = " + resultado);
+}
